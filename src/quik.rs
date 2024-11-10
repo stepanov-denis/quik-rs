c735fe82458b25140b396a6ca9a08b088121e685
stepanov-denis <stepanov.d.m@yandex.ru>
2024-11-11 00:12:18 +0700

    add enum Trans2QuikError

--- src/quik.rs
+++ src/quik.rs
@@ -4,9 +4,9 @@
 //! the functionality is implemented through the API in the form of a library Trans2QUIK.dll .
 //!
 //! The library contains functions, when calling these functions, you can:
-//! * Establish or break the connection between the QUIK Workplace and the library 
+//! * Establish or break the connection between the QUIK Workplace and the library
 //! Trans2QUIK.dll
-//! * Check if there is a connection between the QUIK Workplace and the library 
+//! * Check if there is a connection between the QUIK Workplace and the library
 //! Trans2QUIK.dll and between the QUIK Workplace and the QUIK server.
 //! * Send the transaction.
 //! * Get information on applications and transactions.
@@ -18,7 +18,7 @@
 //! can only be sent sequentially, waiting for a response about each sent transaction –
 //! this method is simpler and more suitable for programmers with little
 //!software development experience.
-//! * With asynchronous transaction transfer, the function is exited immediately. 
+//! * With asynchronous transaction transfer, the function is exited immediately.
 //! The callback function is used to receive a response about sent asynchronous transactions.
 //! The function is called every time a response
 //! is received about an executed or rejected transaction.
@@ -27,17 +27,19 @@
 //! the QUIK terminal and the library Trans2QUIK.dll and between the QUIK Workplace
 //! and the QUIK server.
 //!
-//! To receive information about orders and transactions, the user must first 
-//! create a list of received instruments, separately for applications and transactions. Then 
+//! To receive information about orders and transactions, the user must first
+//! create a list of received instruments, separately for applications and transactions. Then
 //! the procedure for obtaining information using the callback functions is started.
 //! Upon termination of receiving information on applications and transactions, the lists
 //! of received instruments are cleared.
 #![allow(dead_code)]
 use libc::{c_char, c_double, c_long, c_ulonglong, intptr_t};
-use libloading::{Library, Symbol};
-use std::ffi::CStr;
-use std::ffi::CString;
+use libloading::{Error as LibloadingError, Library, Symbol};
+use std::error;
+use std::ffi::{CStr, CString, NulError};
+use std::fmt;
 use std::str;
+use std::string::FromUtf8Error;
 use tracing::{error, info};
 
 /// Prototype of a callback function for status monitoring connections.
@@ -61,7 +63,7 @@ type Trans2QuikTransactionReplyCallback = unsafe extern "C" fn(
 );
 
 /// Corresponds to the description of constants whose values are returned when exiting functions
-/// and procedures in the library `Trans2QUIK.dll `:
+/// and procedures in the library Trans2QUIK.dll:
 /// ```
 /// TRANS2QUIK_SUCCESS 0
 /// TRANS2QUIK_FAILED 1
@@ -100,8 +102,6 @@ pub enum Trans2QuikResult {
     Unknown,
 }
 
-/// Implementation From<c_long> for Trans2QuikResult,
-/// to automatically convert the integer code to the appropriate enumeration variant:
 impl From<c_long> for Trans2QuikResult {
     fn from(code: c_long) -> Self {
         match code {
@@ -125,27 +125,36 @@ impl From<c_long> for Trans2QuikResult {
     }
 }
 
-/// Loads the symbol from the library Trans2QUIK.dll
-fn load_symbol<T>(library: &Library, name: &[u8]) -> Result<T, Box<dyn std::error::Error>>
-where
-    T: Copy,
-{
-    unsafe {
-        let symbol: Symbol<T> = library.get(name).map_err(|e| {
-            error!(
-                "Load '{}' from `Trans2QUIK.dll` error: {}",
-                str::from_utf8_unchecked(name),
-                e
-            );
-            e
-        })?;
-        Ok(*symbol)
+/// Сomposite error type for calling functions from the library Trans2QUIK.dll.
+#[derive(Debug)]
+pub enum Trans2QuikError {
+    LibLoading(LibloadingError),
+    NulError(NulError),
+}
+
+impl fmt::Display for Trans2QuikError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
-/// The `Terminal` structure is used to interact with the QUIK trading terminal through the library `Trans2QUIK.dll`.
+impl error::Error for Trans2QuikError {}
+
+impl From<LibloadingError> for Trans2QuikError {
+    fn from(err: LibloadingError) -> Trans2QuikError {
+        Trans2QuikError::LibLoading(err)
+    }
+}
+
+impl From<NulError> for Trans2QuikError {
+    fn from(err: NulError) -> Trans2QuikError {
+        Trans2QuikError::NulError(err)
+    }
+}
+
+/// The `Terminal` structure is used to interact with the QUIK trading terminal through the library Trans2QUIK.dll.
 ///
-/// This structure provides loading of the DLL library `Trans2QUIK.dll `, establishing a connection to the QUIK terminal
+/// This structure provides loading of the DLL library Trans2QUIK.dll, establishing a connection to the QUIK terminal
 /// and calling functions from the library to control the terminal and perform trading operations.
 ///
 /// # Example of use
@@ -155,7 +164,7 @@ where
 /// terminal.connect()?;
 /// ```
 pub struct Terminal {
-    /// Loading a dynamic library Trans2QUIK.dll , which provides an API for interacting with QUIK.
+    /// Loading a dynamic library Trans2QUIK.dll, which provides an API for interacting with QUIK.
     library: Library,
 
     /// Calling a function from the library Trans2QUIK.dll for establishing communication with the QUIK terminal.
@@ -211,8 +220,8 @@ pub struct Terminal {
 
 impl Terminal {
     /// The function is used to load the library Trans2QUIK.dll.
-    pub fn new(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        // Loading a dynamic library `Trans2QUIK.dll `, which provides an API for interacting with QUIK.
+    pub fn new(path: &str) -> Result<Self, Trans2QuikError> {
+        // Loading a dynamic library Trans2QUIK.dll, which provides an API for interacting with QUIK.
         let library = unsafe { Library::new(path)? };
 
         // Calling a function from the library Trans2QUIK.dll for establishing communication with the QUIK terminal.
@@ -301,7 +310,7 @@ impl Terminal {
         &self,
         function_name: &str,
         func: F,
-    ) -> Result<Trans2QuikResult, Box<dyn std::error::Error>>
+    ) -> Result<Trans2QuikResult, Trans2QuikError>
     where
         F: FnOnce(*mut c_long, *mut c_char, c_long) -> c_long,
     {
@@ -336,7 +345,7 @@ impl Terminal {
     }
 
     /// The function is used to establish communication with the QUIK terminal.
-    pub fn connect(&self) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn connect(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let connection_str = CString::new(r"c:\QUIK Junior")?;
         let connection_str_ptr = connection_str.as_ptr() as *const c_char;
 
@@ -355,7 +364,7 @@ impl Terminal {
     }
 
     /// The function is used to disconnect from the QUIK terminal.
-    pub fn disconnect(&self) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn disconnect(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let function = |error_code: *mut c_long,
                         error_message: *mut c_char,
                         error_message_len: c_long| unsafe {
@@ -366,7 +375,7 @@ impl Terminal {
     }
 
     /// The function is used to check if there is a connection between the QUIK terminal and the server.
-    pub fn is_quik_connected(&self) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn is_quik_connected(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let function = |error_code: *mut c_long,
                         error_message: *mut c_char,
                         error_message_len: c_long| unsafe {
@@ -377,7 +386,7 @@ impl Terminal {
     }
 
     /// Checking for a connection between the library Trans2QUIK.dll and the QUIK terminal.
-    pub fn is_dll_connected(&self) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn is_dll_connected(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let function = |error_code: *mut c_long,
                         error_message: *mut c_char,
                         error_message_len: c_long| unsafe {
@@ -393,7 +402,7 @@ impl Terminal {
     pub fn send_sync_transaction(
         &self,
         transaction_str: &str,
-    ) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    ) -> Result<Trans2QuikResult, Trans2QuikError> {
         let trans_str = CString::new(transaction_str).expect("CString::new failed");
         let trans_str_ptr = trans_str.as_ptr() as *const c_char;
 
@@ -466,7 +475,7 @@ impl Terminal {
     pub fn send_async_transaction(
         &self,
         transaction_str: &str,
-    ) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    ) -> Result<Trans2QuikResult, Trans2QuikError> {
         let trans_str = CString::new(transaction_str).expect("CString::new failed");
         let trans_str_ptr = trans_str.as_ptr() as *const c_char;
 
@@ -504,9 +513,7 @@ impl Terminal {
     }
 
     /// А callback function for processing the received connection information.
-    pub fn set_connection_status_callback(
-        &self,
-    ) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn set_connection_status_callback(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let mut error_code: c_long = 0;
         let error_code_ptr = &mut error_code as *mut c_long;
 
@@ -540,9 +547,7 @@ impl Terminal {
     }
 
     /// Sets the callback function to receive information about the sent asynchronous transaction.
-    pub fn set_transactions_reply_callback(
-        &self,
-    ) -> Result<Trans2QuikResult, Box<dyn std::error::Error>> {
+    pub fn set_transactions_reply_callback(&self) -> Result<Trans2QuikResult, Trans2QuikError> {
         let mut error_code: c_long = 0;
         let error_code_ptr = &mut error_code as *mut c_long;
 
@@ -577,8 +582,19 @@ impl Terminal {
     }
 }
 
+/// Loads the symbol from the library Trans2QUIK.dll
+fn load_symbol<T>(library: &Library, name: &[u8]) -> Result<T, LibloadingError>
+where
+    T: Copy,
+{
+    unsafe {
+        let symbol: Symbol<T> = library.get(name)?;
+        Ok(*symbol)
+    }
+}
+
 /// Extract String from `Vec<i8>`.
-fn extract_string_from_vec(vec_i8: Vec<i8>) -> Result<String, std::string::FromUtf8Error> {
+fn extract_string_from_vec(vec_i8: Vec<i8>) -> Result<String, FromUtf8Error> {
     let vec_u8: Vec<u8> = vec_i8.into_iter().map(|byte| byte as u8).collect();
 
     let null_pos = vec_u8
